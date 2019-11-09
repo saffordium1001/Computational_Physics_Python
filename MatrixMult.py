78cd5cb5f037ffe6d351a23c0b6c8b41bc021bd7
saffordium1001 <42842364+saffordium1001@users.noreply.github.com>
2019-11-09 11:43:17 -0500

    Create MatrixMult.py

---
+++ MatrixMult.py
@@ -0,0 +1,14 @@
+ # Matrix multiplication using visual arrays
+ 
+from visual import *                   
+
+vector1 = array([1, 2, 3, 4, 5])       # Fill array
+print(' vector1 =',vector1)              
+vector2 = vector1 + vector1            # Add vectors
+print('\n vector2 =',vector2)               
+vector2 = 3 * vector1                  # Mult array by scalar
+print ('\n 3 * vector1 = ', vector2)       
+matrix1 = array(([0,1],[1,3]))         # An array of arrays
+print('\n matrix1 = \n', matrix1)                          
+print ('\n vector1.shape= ',vector1.shape)
+print ('\n matrix1 * matrix1 =\n', matrix1 * matrix1)     # Matrix multiply
